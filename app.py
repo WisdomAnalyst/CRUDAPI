514809cf40195f5ee237dfb1ed86f9b01beacc66
WisdomAnalyst <domwicommercial@gmail.com>
2026-07-18 18:18:38 +0100

    Stage 0: hello Flyrank

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app=Flask(__name__)
+
+@app.route('/')
+
+def hello():
+    return "Hello, Flyrank!"
+     
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
