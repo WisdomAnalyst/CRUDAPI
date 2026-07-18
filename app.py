05220ce6a758de96ade4ee9fa8b09485b2f72594
WisdomAnalyst <domwicommercial@gmail.com>
2026-07-18 18:52:53 +0100

    Stage 1: root and health endpoints

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 
-def hello():
-    return "Hello, Flyrank!"
+def home():
+    return jsonify(
+        {"name": "Task API", 
+         "version": "1.0", 
+         "endpoints": ["/tasks"]})
      
+     
+     
+app.route('/health')
+def health():
+    return jsonify({"status": "ok"})
+    
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
